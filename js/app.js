ffda0410ffaa5ea39c3897f6558eb0e810d97a92
raksha24ad047 <rakshashetty2456@gmail.com>
2026-09-19 12:58:45 +0530

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,20 @@
+import { startRouter } from "./router.js";
+
+
+/* ========================================
+   SHOPSPHERE APPLICATION
+   ======================================== */
+
+function initializeApp() {
+
+    console.log("ShopSphere application started!");
+
+    startRouter();
+}
+
+
+/* ========================================
+   START APPLICATION
+   ======================================== */
+
+initializeApp();
